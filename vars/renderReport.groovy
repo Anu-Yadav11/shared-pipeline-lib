1b67b42cdd015b981e6f0420a0c62c39c81614f1
Anu-Yadav11 <49764164+Anu-Yadav11@users.noreply.github.com>
2025-09-19 22:41:40 +0530

    Create renderReport.groovy

---
+++ vars/renderReport.groovy
@@ -0,0 +1,10 @@
+def call(Map args = [:]) {
+  String project = args.project ?: 'unknown'
+  String branch = args.branch ?: env.BRANCH_NAME ?: 'local'
+  String result = args.result ?: 'UNKNOWN'
+  def r = new com.example.ReportUtils(this)
+  def txt = r.render(project, branch, result)
+  writeFile file: 'build-report.txt', text: txt
+  archiveArtifacts artifacts: 'build-report.txt'
+  return txt
+}
