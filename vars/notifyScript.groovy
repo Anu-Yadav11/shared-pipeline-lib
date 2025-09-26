2cfeaca84c0e6911316e33ffc5da74c200c638d6
Anu-Yadav11 <49764164+Anu-Yadav11@users.noreply.github.com>
2025-09-26 20:23:37 +0530

    Create notifyScript.groovy

---
+++ vars/notifyScript.groovy
@@ -0,0 +1,60 @@
+def call(Map opts = [:] ){
+    def name = opts.name ?: 'Unknown'
+    def day = opts.day ?: '??'
+    def month = opts.month ?: '??'
+    def timing = opts.shift_timing ?: 'Not given'
+    def status = opts.shift_status ?: 'start'
+
+    def msg = """${name} shift ${status}ed
+    Day: ${day}, Month: ${month}
+Timing: ${timing}
+"""
+  //Save shift report
+
+  def report ="""\
+   shift Report
+   =============
+    Person : ${name}
+    Day    : ${day}
+    Month  : ${month}
+    Timing : ${timing}
+    Status : ${status.toUpperCase()}
+    Job    : ${env.JOB_NAME}
+    Build  : ${env.BUILD_NUMBER}
+    URL    : ${env.BUILD_URL}
+    """
+    writeFile file: 'shift-report.txt', text: report
+    archiveArtifacts artifacts: 'shift-report.txt'
+
+    // Append to history log
+    sh """echo '${report}' >> shift-report.log"""
+    archiveArtifacts artifacts: 'shift-history.log'
+
+    //telegram block start
+
+    def botToken = "8272985598:AAFZ33GjxAKChoNkveXYNcRX-6hsyAbFtUM"
+    def chatId = "-4870913458"
+    sh """
+    curl -s -X POST https://api.telegram.org/bot${botToken}/sendMessage \
+    -d chat_id=${chatId} \
+    -d text="${msg}"
+  """
+    def jsonDoc = """{
+    "name": "${name}",
+    "day": "${day}",
+    "month": "${month}",
+    "timing": "${timing}",
+    "status": "${status}",
+    "job": "${env.JOB_NAME}",
+    "build": "${env.BUILD_NUMBER}",
+    "url": "${env.BUILD_URL}",
+    "timestamp": "${new Date().format('yyyy-MM-dd HH:mm:ss')}"
+  }"""
+
+    sh """
+    echo '${jsonDoc}' | mongosh "mongodb://admin:admin@localhost:27017/shiftsDB" --quiet --eval 'const doc=JSON.parse(cat("/dev/stdin")); db.shifts.insertOne(doc);'
+  """
+
+  echo "Shift notification + DB record saved for ${name}"
+
+}
