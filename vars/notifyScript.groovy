e16396ae10246cc55139ed111b4c28470ed28f9e
Anu-Yadav11 <49764164+Anu-Yadav11@users.noreply.github.com>
2025-09-27 13:58:27 +0530

    Update notifyScript.groovy

--- vars/notifyScript.groovy
+++ vars/notifyScript.groovy
@@ -44,7 +44,7 @@ URL    : ${env.BUILD_URL}
 
     // ----- MongoDB insert/update with idempotency -----
   def now = new Date().format('yyyy-MM-dd HH:mm:ss')
-  def mongoOutput = ""
+  def mongoOutput = " "
 
   if (status == "start") {
     mangoOutput = sh (
